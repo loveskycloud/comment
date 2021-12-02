37fdb9ac30ba5678d9a87b330ace8982f38a80cb
loveskycloud <896275569@qq.com>
2021-12-02 16:28:35 +0800

    a.cpp

---
+++ a.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: a.cpp
+	> Author: wangsheng
+	> Mail: wangsheng716@163.com
+	> Created Time: Thu 02 Dec 2021 04:28:14 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << 666 << endl;
+    return 0;
+}
