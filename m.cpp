3bc6675f113f4d21478efa2463199e54f33afe9b
loveskycloud <896275569@qq.com>
2021-12-04 19:29:37 +0800

    add m.cpp

---
+++ m.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: m.cpp
+	> Author: wangsheng
+	> Mail: wangsheng716@163.com
+	> Created Time: Sat 04 Dec 2021 07:29:13 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << 666 << endl;
+    return 0;
+}
